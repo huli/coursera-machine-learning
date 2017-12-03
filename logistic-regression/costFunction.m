19d423e77691ed3c8ddaeac0af7dda6ad391e2e6
huli <huli@eitschei.com>
2017-12-03 15:34:40 +0100

    Implemented sigmoid and cost function.

--- logistic-regression/costFunction.m
+++ logistic-regression/costFunction.m
@@ -11,22 +11,6 @@ m = length(y); % number of training examples
 J = 0;
 grad = zeros(size(theta));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
-%
-% Note: grad should have the same dimensions as theta
-%
-
-
-
-
-
-
-
-
-% =============================================================
+J = (1 / m) * sum((-y .* log(sigmoid(X * theta)) - (1- y) .* log(1- sigmoid(X * theta)))); 
 
 end
