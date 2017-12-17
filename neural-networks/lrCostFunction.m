f078a64ddd551a4ab8d9cdbe4923832275627107
Christoph Hilty <christoph.hilty@garaio.com>
2017-12-17 14:42:00 +0100

    Vectorized, reqularized cost function.

--- neural-networks/lrCostFunction.m
+++ neural-networks/lrCostFunction.m
@@ -12,41 +12,13 @@ m = length(y); % number of training examples
 J = 0;
 grad = zeros(size(theta));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
-%
-% Hint: The computation of the cost function and gradients can be
-%       efficiently vectorized. For example, consider the computation
-%
-%           sigmoid(X * theta)
-%
-%       Each row of the resulting matrix will contain the value of the
-%       prediction for that example. You can make use of this to vectorize
-%       the cost function and gradient computations. 
-%
-% Hint: When computing the gradient of the regularized cost function, 
-%       there're many possible vectorized solutions, but one solution
-%       looks like:
-%           grad = (unregularized gradient for logistic regression)
-%           temp = theta; 
-%           temp(1) = 0;   % because we don't add anything for j = 0  
-%           grad = grad + YOUR_CODE_HERE (using the temp variable)
-%
+J = (1 / m) * sum((-y .* log(sigmoid(X * theta)) ...
+    - (1- y) .* log(1- sigmoid(X * theta)))) ...
+    + (lambda / (2 * m)) * sum( theta(2:length(theta), :) .^2); 
 
 
-
-
-
-
-
-
-
-
-% =============================================================
-
-grad = grad(:);
+thetaZero = [ [ 0 ]; theta([2:length(theta)])];
+grad = (1/m) * sum((sigmoid(X * theta) - y) .* X) ...
+        + (lambda / m) * thetaZero';
 
 end
