d59f94a84d5d6ab373e36f585fd550622182d4d3
Christoph Hilty <christoph.hilty@garaio.com>
2017-12-18 22:57:01 +0100

    Predict for oneVsAll implemented.

--- neural-networks/predictOneVsAll.m
+++ neural-networks/predictOneVsAll.m
@@ -17,26 +17,7 @@ p = zeros(size(X, 1), 1);
 % Add ones to the X data matrix
 X = [ones(m, 1) X];
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Complete the following code to make predictions using
-%               your learned logistic regression parameters (one-vs-all).
-%               You should set p to a vector of predictions (from 1 to
-%               num_labels).
-%
-% Hint: This code can be done all vectorized using the max function.
-%       In particular, the max function can also return the index of the 
-%       max element, for more information see 'help max'. If your examples 
-%       are in rows, then, you can use max(A, [], 2) to obtain the max 
-%       for each row.
-%       
-
-
-
-
-
-
-
-% =========================================================================
-
+all_models = X * all_theta';
+[x, p] = max(all_models, [], 2);
 
 end
