84b8332f33df27df07c954edf670a4c2f4421a93
huli <huli@eitschei.com>
2017-12-30 16:11:03 +0100

    Initialized assignment for week 6.

---
+++ bias-variance/linearRegCostFunction.m
@@ -0,0 +1,19 @@
+function [J, grad] = linearRegCostFunction(X, y, theta, lambda)
+%LINEARREGCOSTFUNCTION Compute cost and gradient for regularized linear 
+%regression with multiple variables
+%   [J, grad] = LINEARREGCOSTFUNCTION(X, y, theta, lambda) computes the 
+%   cost of using theta as the parameter for linear regression to fit the 
+%   data points in X and y. Returns the cost in J and the gradient in grad
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+grad = zeros(size(theta));
+
+
+
+grad = grad(:);
+
+end
