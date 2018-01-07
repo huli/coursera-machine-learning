5c4d9897a56e196dd8150a747752c37a5cbf49ae
huli <huli@eitschei.com>
2018-01-07 18:31:18 +0100

    Implemented evaluation of different meta parameters.

--- support-vector-machines/dataset3Params.m
+++ support-vector-machines/dataset3Params.m
@@ -7,28 +7,31 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %   sigma based on a cross-validation set.
 %
 
-% You need to return the following variables correctly.
-C = 1;
-sigma = 0.3;
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Fill in this function to return the optimal C and sigma
-%               learning parameters found using the cross validation set.
-%               You can use svmPredict to predict the labels on the cross
-%               validation set. For example, 
-%                   predictions = svmPredict(model, Xval);
-%               will return the predictions on the cross validation set.
-%
-%  Note: You can compute the prediction error using 
-%        mean(double(predictions ~= yval))
-%
+errors = zeros(64, 3);
+values = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
 
+counter = 1;
+for c = 1:8
+    for s = 1:8
+        
+        % train model with current parameters on training set
+        model= svmTrain(X, y, values(c), @(x1, x2) gaussianKernel(x1, x2, values(s)));
+        
+        % predict classes on cross validation set
+        predictions = svmPredict(model, Xval);
+        
+        % calculate current error and store with parameters
+        errors(counter, :) = [values(c) values(s) mean(double(predictions ~= yval))];
+        
+        counter += 1;
+    endfor
+endfor
 
+% get parameters of smalles errors
+[val, index] = min(errors(:, 3))
 
-
-
-
-
-% =========================================================================
+C = errors(index, 1);
+sigma = errors(index, 2);
 
 end
